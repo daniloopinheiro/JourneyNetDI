6f14aeaa2323eb7ed122941adfc4d9f6df71d611
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-07-06 01:23:19 -0300

    Update WeatherforecastEndpoint.cs

--- src/DependencyInjection.API/Endpoints/Weatherforecast/WeatherforecastEndpoint.cs
+++ src/DependencyInjection.API/Endpoints/Weatherforecast/WeatherforecastEndpoint.cs
@@ -6,7 +6,7 @@ public static class WeatherforecastEndpoint
     {
         var summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         app.MapGet("api/weatherforecast", () =>
